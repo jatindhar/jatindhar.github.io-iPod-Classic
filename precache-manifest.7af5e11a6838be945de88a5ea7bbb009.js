6e3ac0a7a7f87a813e00a7578314068b5dcc9526
Jatin Dhar <jatindhar350@gmail.com>
2021-02-14 20:07:57 +0530

    Updates

---
+++ precache-manifest.7af5e11a6838be945de88a5ea7bbb009.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc26e3ab727cdbbb7db69798fe2188f0",
+    "url": "/iPod-Classic/index.html"
+  },
+  {
+    "revision": "fe1f708015359d7d6481",
+    "url": "/iPod-Classic/static/css/main.7a277625.chunk.css"
+  },
+  {
+    "revision": "62c0b81ec1c0bdc043ef",
+    "url": "/iPod-Classic/static/js/2.27b9ed72.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/iPod-Classic/static/js/2.27b9ed72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1f708015359d7d6481",
+    "url": "/iPod-Classic/static/js/main.6606b489.chunk.js"
+  },
+  {
+    "revision": "4deacc06a44225f75abb",
+    "url": "/iPod-Classic/static/js/runtime-main.6904ec8c.js"
+  },
+  {
+    "revision": "29db525ea81ef4906441f58f82289d44",
+    "url": "/iPod-Classic/static/media/Alan Walker ‒ The Spectre ( ).29db525e.mp3"
+  },
+  {
+    "revision": "1836a35e398ef6b6d0a374d044217674",
+    "url": "/iPod-Classic/static/media/K-391.1836a35e.mp3"
+  },
+  {
+    "revision": "5f52455b0197ee975f5d192ff9956328",
+    "url": "/iPod-Classic/static/media/Unknown Brain - Why Do I ft. Bri Tolani.5f52455b.mp3"
+  },
+  {
+    "revision": "ab7c69c2c47e3cbd52c55486514eb1d3",
+    "url": "/iPod-Classic/static/media/earth.ab7c69c2.jpg"
+  },
+  {
+    "revision": "ec42ead05165b16b9d0c011d0966c4c2",
+    "url": "/iPod-Classic/static/media/game.ec42ead0.jpg"
+  },
+  {
+    "revision": "3f6adda346c175fc14a16335a843da05",
+    "url": "/iPod-Classic/static/media/jatin.3f6adda3.jpeg"
+  },
+  {
+    "revision": "494ed9c0922a3ee87ef2569f5b29d8fe",
+    "url": "/iPod-Classic/static/media/jupiter.494ed9c0.png"
+  },
+  {
+    "revision": "afb8461f9cdaa8339e73d5be2439e454",
+    "url": "/iPod-Classic/static/media/moon.afb8461f.jpg"
+  },
+  {
+    "revision": "5d8a23839f98efec957c488457eea93e",
+    "url": "/iPod-Classic/static/media/settings.5d8a2383.png"
+  },
+  {
+    "revision": "b243b63f611b5366b38a5df15791c0ef",
+    "url": "/iPod-Classic/static/media/spectre.b243b63f.jpg"
+  },
+  {
+    "revision": "6d555f179047445fcbebac05b8da3bfb",
+    "url": "/iPod-Classic/static/media/whydoi.6d555f17.jpg"
+  }
+]);
